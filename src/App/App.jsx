bf67b202b97f65081fd774a503613bcd002cb99f
FlavS41 <cezarsalxz41@gmail.com>
2023-06-06 23:01:55 -0400

    added skeleton components

---
+++ src/App/App.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+
+export const App = () => {
+  const [nav, setNav] = useState(0);
+  const [clicked, setClicked] = useState();
+  const [events, setEvents] = useState(
+    localStorage.getItem("events")
+      ? JSON.parse(localStorage.getItem("events"))
+      : []
+  );
+};
