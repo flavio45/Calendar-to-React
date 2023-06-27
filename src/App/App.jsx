f5281a9d43362466e64f873408b169b8b61dcc8e
FlavS41 <cezarsalxz41@gmail.com>
2023-06-27 09:43:54 -0400

    Added CalendarHeader

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -17,4 +17,19 @@ export const App = () => {
   useEffect(() => {
     localStorage.setItem("events", JSON.stringify(events));
   }, [events]);
+
+  return (
+    <div id="container">
+      <div id="weekdays">
+        <div>Sunday</div>
+        <div>Monday</div>
+        <div>Tuesday</div>
+        <div>Wednesday</div>
+        <div>Thursday</div>
+        <div>Friday</div>
+        <div>Saturday</div>
+      </div>
+      <div id="calendar"></div>
+    </div>
+  );
 };
