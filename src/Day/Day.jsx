19e9a39b45b1cc70aface859a658ee24fcc6d9d0
FlavS41 <cezarsalxz41@gmail.com>
2023-06-27 10:40:21 -0400

    added to Day component

--- src/Day/Day.jsx
+++ src/Day/Day.jsx
@@ -1,5 +1,14 @@
-import React from 'react';
+import React from "react";
 
 export const Day = () => {
-    return ();
-}
+  return (
+    <div id="header">
+      <div id="monthDisplay"></div>
+
+      <div>
+        <button id="backButton">Back</button>
+        <button id="nextButton">Next</button>
+      </div>
+    </div>
+  );
+};
