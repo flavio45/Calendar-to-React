7b493593aae614311741d91d045ecff6c00825d3
FlavS41 <cezarsalxz41@gmail.com>
2023-06-27 09:25:13 -0400

    Added useStates

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,11 +1,20 @@
 import React, { useState } from "react";
+import React, { useEffect } from "react";
 
 export const App = () => {
   const [nav, setNav] = useState(0);
+  const [days, setDays] = useState([]);
+  const [dateDisplay, setDateDisplay] = useState("");
   const [clicked, setClicked] = useState();
   const [events, setEvents] = useState(
     localStorage.getItem("events")
       ? JSON.parse(localStorage.getItem("events"))
       : []
   );
+
+  const eventForDate = (date) => events.find((e) => e.date === date);
+
+  useEffect(() => {
+    localStorage.setItem("events", JSON.stringify(events));
+  }, [events]);
 };
