7a48b1305b2683909dd55a3034051cda06f67ce6
FlavS41 <cezarsalxz41@gmail.com>
2023-06-28 10:16:23 -0400

    new events built out

---
+++ src/NewEvent/NewEvent.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+
+export const NewEvent = ({ onSave, onClose }) => {
+  const [title, setTitle] = useState("");
+  const [error, setError] = useState(false);
+
+  return (
+    <>
+      <div id="newEventModal">
+        <h2>New Event</h2>
+        <input
+          className={error ? "error" : ""}
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          id="eventTitleInput"
+          placeholder="Event Title"
+        />
+
+        <button
+          onClick={() => {
+            if (title) {
+              setError(false);
+            } else {
+              setError(true);
+            }
+          }}
+          id="saveButton"
+        >
+          Save
+        </button>
+        <button onClick={onClose} id="cancelButton">
+          Cancel
+        </button>
+      </div>
+
+      <div id="modalBackdrop"></div>
+    </>
+  );
+};
